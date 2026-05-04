0f60324db3c5548c1a0f1d2be33248bcc1ab066c
Alassane Kaba <alassane.kaba@externe.systeme-u.fr>
2026-05-04 20:02:16 +0200

    fix: call of init_db()

--- app.py
+++ app.py
@@ -560,8 +560,8 @@ def init_db():
         cur.close()
         db.close()
 
+init_db()
 
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
